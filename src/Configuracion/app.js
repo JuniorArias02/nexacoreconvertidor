76da2b3a6667433263bae115d0ac9d0a7a42519d
Junior Arias <junior.arias02yt@gmail.com>
2026-06-17 11:13:06 -0500

    correcion de webSocket

--- src/Configuracion/app.js
+++ src/Configuracion/app.js
@@ -17,10 +17,17 @@ configurarDocumentacion(aplicacion);
 // Si agregas más módulos en el futuro, importa sus rutas aquí.
 aplicacion.use('/api', rutasConvertidor);
 
+import path from 'path';
+
 // Endpoint visual de estado del servidor (Health Check Frontend)
 aplicacion.get('/api/state', EstadoControlador.obtenerVista);
 
 // Endpoint de datos para la telemetría (Health Check Backend JSON)
 aplicacion.get('/api/state/data', EstadoControlador.obtenerDatos);
+
+// Endpoint interno para proveer el cliente WebSocket evadiendo bloqueos de router y CDNs
+aplicacion.get('/api/recursos/socket.js', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'node_modules', 'socket.io', 'client-dist', 'socket.io.min.js'));
+});
 
 export default aplicacion;
